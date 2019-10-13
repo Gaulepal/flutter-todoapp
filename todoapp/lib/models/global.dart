f0e1ef3bb7571aae14c0fc51eb43a87202c26cf7
Gaulepal <gaulepal@gmail.com>
2019-10-13 15:36:32 +0200

    Create intray topbar and button #2
    Good so far

---
+++ todoapp/lib/models/global.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+Color darkRedColor = Color(0xFFf75f00);
+Color lightRedColor = Color(0xFFf75f00);
+Color darkCyanColor = Color(0xFF40bfc1);
+Color lightGreyColor = Color(0xFFe5e5e5);
+Color darkGreyColor = Color(0xFF5d5d5d);
+Color lightClayColor = Color(0xFFf6ECBF);
+Color pureBlackColor = Color(0xFF000000);
+
+TextStyle intrayTitleStyle = TextStyle(
+  fontFamily: 'Avenir',
+  fontWeight: FontWeight.bold,
+  color: pureBlackColor,
+  fontSize: 40.0,
+);
+
+TextStyle listingHeading = TextStyle(
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+  color: darkGreyColor,
+  fontFamily: 'Avenir',
+);
+
+TextStyle listSubheading = TextStyle(
+  fontFamily: 'Avenir',
+  color: lightGreyColor,
+  fontSize: 20.0,
+);
